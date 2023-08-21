1caf13987d2518637364211b18ffb3a377123645
Abdela Umer <adheremo65@gmail.com>
2023-08-20 21:06:04 -0400

    add foreign key

--- lib/db/debug.py
+++ lib/db/debug.py
@@ -5,15 +5,25 @@ import random
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from models import User, Expense
+from models import User,Expense 
 
 fake = Faker()
 
 if __name__ == '__main__':
     
-    engine = create_engine('sqlite:///expenses.db')
+    engine = create_engine('sqlite:///:expenses.db')
     Session = sessionmaker(bind=engine)
     session = Session()
+
+    user = User(user_name="JohnDoe", password= 123456)
+    session.add(user)
+    session.commit()
+
+    expense = Expense(item="Item 1", size="Medium", quantity=2, price=10.0, total=20,user_id = 1)
+    session.add(expense)
+    session.commit()
+
+    
 
 
     import ipdb; ipdb.set_trace()
---
+++ lib/db/seed.py
@@ -0,0 +1,4 @@
+from models import users, expenses
+from debug import debug
+
+

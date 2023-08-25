2fe8cb307206d2342b1557998fa0b103de4ab2ec
Abdela Umer <adheremo65@gmail.com>
2023-08-24 23:22:52 -0400

    log in authontication add

--- lib/db/cli.py
+++ lib/db/cli.py
@@ -4,19 +4,19 @@ from colorama import init, Fore, Style
 from sqlalchemy.orm import sessionmaker
 engine = create_engine("sqlite:///expense.db")
 session = sessionmaker(bind=engine)
-current_user = None
+
 init()
 
 
 class display:
-
+    current_user = None
 
     def login(self):
         user_name = input("input your name")
         password = input("put your password")
         user = session.query(User).filter_by(user_name = user_name, password = password).first()
         if user:
-            current_user = user
+            self.current_user = user
             print(f"Welcome, {user.user_name}!")
         else:
             print("Invalid credentials. Please try again.")
@@ -25,4 +25,8 @@ class display:
         print(text + color +Style.RESET_ALL )
 
     def totat_expense(self):
-        pass
+        if self.current_user:
+            total_expense = session.query(func.sum(Expense.total)).filter_by(user_id= self.current_user.id).scalar()
+            print(f"your total expense is:  {total_expense}")
+        else:
+           print("Please log in first.")
